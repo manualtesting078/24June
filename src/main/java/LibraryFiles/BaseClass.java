191fa87b9c3e7c19507e8d0416d036ff37b3e487
LENOVO <LENOVO@DESKTOP-36OJA94>
2023-12-14 02:42:14 +0530

    [mayur]24June all program

--- src/main/java/LibraryFiles/BaseClass.java
+++ src/main/java/LibraryFiles/BaseClass.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class BaseClass 
 {
